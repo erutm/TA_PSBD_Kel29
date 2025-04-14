727a867a3273ddcbcd3d183a020c48e2bdc69feb
Safira Septiandika Salsabila <114891867+safirass@users.noreply.github.com>
2025-04-14 18:44:36 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -21,29 +21,34 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/',[FrontpageController::class, 'index']);
-Route::get('/login',[LoginController::class, 'login']);
+Route::get('/login',[LoginController::class, 'login'])->name('login');
 Route::post('/login',[LoginController::class, 'authenticate']);
-Route::post('/logout', [LoginController::class, 'logout']);
+Route::post('/logout', [LoginController::class, 'logout'])->middleware('auth');
 Route::get('/about',[FrontpageController::class, 'about']);
-Route::get('/dashboard', function(){return view('dashboard.index');});
-Route::get('/dashboard/reservasis/recycle', [DashboardReservasiController::class, 'recycle']);
 Route::get('/product', [FrontpageController::class, 'product']);
-Route::post('/dashboard/pakets/delete/{paket}', [DashboardPaketController::class, 'delete']);
-Route::get('/dashboard/pakets/restore/{paket}', [DashboardPaketController::class, 'restore']);
-Route::post('/dashboard/barbers/delete/{barber}', [DashboardBarberController::class, 'delete']);
-Route::get('/dashboard/barbers/restore/{barber}', [DashboardBarberController::class, 'restore']);
-Route::post('/dashboard/reservasis/delete/{reservasi}', [DashboardReservasiController::class, 'delete']);
-Route::get('/dashboard/reservasis/restore/{reservasi}', [DashboardReservasiController::class, 'restore']);
-Route::get('/dashboard/pakets/recycle', [DashboardPaketController::class, 'recycle']);
-Route::get('/dashboard/barbers/recycle', [DashboardBarberController::class, 'recycle']);
 Route::post('/product/search',[FrontpageController::class,'showProduct'])->name('product.search');
-Route::resource('/dashboard/pakets', DashboardPaketController::class);
-Route::resource('/dashboard/barbers', DashboardBarberController::class);
-Route::resource('/dashboard/reservasis', DashboardReservasiController::class);
 Route::post('/product', [FrontpageController::class, 'beliProduk']);
-Route::get('/dashboard/fakturs/recycle', [DashboardFakturController::class, 'recycle']);
-Route::post('/dashboard/fakturs/delete/{faktur}', [DashboardFakturController::class, 'delete']);
-Route::get('/dashboard/fakturs/restore/{faktur}', [DashboardFakturController::class, 'restore']);
-Route::resource('/dashboard/fakturs', DashboardFakturController::class);
-Route::put('/dashboard/reservasis/{id}', [DashboardReservasiController::class, 'update']);
-Route::get('/dashboard/reservasis', [DashboardReservasiController::class, 'index']);
+
+Route::middleware(['auth'])->group(function () {
+
+    Route::get('/dashboard', function(){return view('dashboard.index');});
+    Route::get('/dashboard/reservasis/recycle', [DashboardReservasiController::class, 'recycle']);
+    Route::post('/dashboard/pakets/delete/{paket}', [DashboardPaketController::class, 'delete']);
+    Route::get('/dashboard/pakets/restore/{paket}', [DashboardPaketController::class, 'restore']);
+    Route::post('/dashboard/barbers/delete/{barber}', [DashboardBarberController::class, 'delete']);
+    Route::get('/dashboard/barbers/restore/{barber}', [DashboardBarberController::class, 'restore']);
+    Route::post('/dashboard/reservasis/delete/{reservasi}', [DashboardReservasiController::class, 'delete']);
+    Route::get('/dashboard/reservasis/restore/{reservasi}', [DashboardReservasiController::class, 'restore']);
+    Route::get('/dashboard/pakets/recycle', [DashboardPaketController::class, 'recycle']);
+    Route::get('/dashboard/barbers/recycle', [DashboardBarberController::class, 'recycle']);
+    Route::resource('/dashboard/pakets', DashboardPaketController::class);
+    Route::resource('/dashboard/barbers', DashboardBarberController::class);
+    Route::resource('/dashboard/reservasis', DashboardReservasiController::class);
+    Route::get('/dashboard/fakturs/recycle', [DashboardFakturController::class, 'recycle']);
+    Route::post('/dashboard/fakturs/delete/{faktur}', [DashboardFakturController::class, 'delete']);
+    Route::get('/dashboard/fakturs/restore/{faktur}', [DashboardFakturController::class, 'restore']);
+    Route::resource('/dashboard/fakturs', DashboardFakturController::class);
+    Route::put('/dashboard/reservasis/{id}', [DashboardReservasiController::class, 'update']);
+    Route::get('/dashboard/reservasis', [DashboardReservasiController::class, 'index']);
+
+});
