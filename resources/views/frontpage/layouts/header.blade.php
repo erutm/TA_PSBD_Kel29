546b485080dcf3e868c69edb7ab03ba7d619ff78
Ery Utami <128942912+erutm@users.noreply.github.com>
2025-04-18 22:00:59 +0700

    Update header.blade.php

--- resources/views/frontpage/layouts/header.blade.php
+++ resources/views/frontpage/layouts/header.blade.php
@@ -1,10 +1,10 @@
-<header class="py-5" style="background-image: linear-gradient(120deg, #d4fc79 0%, #96e6a1 100%);">
-    <div>
-        <div  class="container px-4 px-lg-5 my-5">
-            <div class="text-center text-white">
-                <h1 class="display-4 fw-bolder">Selamat Datang di Barbershop Raka</h1>
-                <p class="lead fw-normal text-white-50 mb-0">Kami bukan tukang cukur, kami arsitek gaya lo 😎</p>
-            </div>
+<header class="py-5 position-relative" style="background-image: linear-gradient(120deg, #d4fc79 0%,rgb(82, 124, 88) 100%); overflow: hidden;">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>    
+<div class="position-absolute top-0 start-0 w-100 h-100" style="background: rgba(0,0,0,0.5); z-index: 1;"></div>
+    <div class="container px-4 px-lg-5 my-5 position-relative" style="z-index: 2;">
+        <div class="text-center text-white">
+            <h1 class="display-3 fw-bold mb-4 animate__animated animate__fadeInDown">Barbershop Raka</h1>
+            <p class="lead fw-normal text-white-50 mb-3 animate__animated animate__fadeInUp">Kami bukan tukang cukur, kami arsitek gaya lo 😎</p>
         </div>
     </div>
 </header>
