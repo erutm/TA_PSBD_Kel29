405622e83eb9f3c73b371866442e47d5dc95bc15
Ery Utami <128942912+erutm@users.noreply.github.com>
2025-04-18 22:02:06 +0700

    Update product.blade.php

--- resources/views/frontpage/pages/product.blade.php
+++ resources/views/frontpage/pages/product.blade.php
@@ -1,5 +1,9 @@
 @extends('frontpage.layouts.main')
 @section('container')
+
+{{-- Header --}}
+@include('frontpage.layouts.header')
+
 <h2 class="mb-3 mt-3 fw-bold text-center">Pilihan Paket Barbershop</h2>
 
 <div class="row justify-content-center mb-3">
@@ -12,44 +16,46 @@
                 <input type="hidden" name="keterangan_paket" value="{{ request('keterangan_paket') }}">
             @endif
             <div class="input-group mb-3">
-                <input type="text" class="form-control" placeholder="Search.." name="search" value={{ request('search') }}>
+                <input type="text" class="form-control" placeholder="Search.." name="search" value="{{ request('search') }}">
                 <button class="btn btn-primary" type="submit">Search</button>
             </div>
         </form>
     </div>
 </div>
 
+{{-- Produk/Card --}}
 <div class="container">
-    <div class="row">
+    <div class="row g-4">
         @foreach($pakets as $paket)
-            @if($paket->id %2 !== 0)
-                <div class="col">
-                    @include('frontpage.components.card')
-                    @include('frontpage.components.modal')
-                </div>
-            @endif
+            <div class="col-12 col-sm-6 col-lg-3 d-flex align-items-stretch">
+                @include('frontpage.components.card')
+                @include('frontpage.components.modal', ['barbers' => $barbers])
+            </div>
         @endforeach
-    </div>
-    <div class="row">
-        @foreach($pakets as $paket)
-            @if($paket->id %2 === 0)
-                <div class="col">
-                    @include('frontpage.components.card')
-                    @include('frontpage.components.modal')
-                </div>
-            @endif
-         @endforeach
     </div>
 </div>
 
+{{-- Pagination --}}
 <div class="d-flex justify-content-center mb-3">
     {{ $pakets->links() }}
 </div>
 
-<script type="text/javascript">
-    $('#search').on('keyup', function(){
-        alert('hello');
-    })
-</script>
-{{-- Modal --}}
+{{-- SweetAlert Sukses --}}
+@if(session('success'))
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script>
+        Swal.fire({
+            icon: 'success',
+            title: 'Reservasi Berhasil!',
+            text: '{{ session("success") }}',
+            showConfirmButton: false,
+            timer: 2000,
+            timerProgressBar: true
+        });
+    </script>
+@endif
+
+{{-- Footer --}}
+@include('frontpage.layouts.footer')
+
 @endsection
